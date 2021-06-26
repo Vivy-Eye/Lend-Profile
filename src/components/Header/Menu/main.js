992188622244bf29322acfac07d9456dc23f3ca4
pin-cup <user@mailfence.com>
2021-06-26 13:13:36 +0300

    Chenge catigories in menu

--- src/components/Header/Menu/main.js
+++ src/components/Header/Menu/main.js
@@ -4,11 +4,11 @@
 const Menu = document.getElementById("menu");
 const Menu_ul = Menu.querySelector(".menu__ul");
 
-["portfolio", "about", "resume", "contact"].forEach((menu_itemText) =>
+["portfolio", "skills", "experience", "education", "about", "contact"].forEach((menu_itemText) =>
   Menu_ul.insertAdjacentHTML(
     "beforeend",
     `
-    <li class="menu__item ${menu_itemText}">
+    <li class="menu__item">
       <a class="menu__item-link" href="#${menu_itemText}">${menu_itemText}</a>
     </li>`
   )

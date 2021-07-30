72206156c4ddd0c528d65a8364163be43bb66d59
pin-cup <user@mailfence.com>
2021-07-30 18:27:48 +0300

    Move data in DB.js

--- src/components/Personal/skills/main.js
+++ src/components/Personal/skills/main.js
@@ -1,76 +1,7 @@
 // ## Skills
 "use strict";
 
-[
-  {
-    name: "HTML",
-    value: 0.884,
-  },
-  {
-    name: "CSS",
-    value: 0.819,
-  },
-  {
-    name: "JavaScript",
-    value: 0.901,
-  },
-  {
-    name: "Node.js",
-    value: 0.291,
-  },
-  {
-    name: "React",
-    value: 0.418,
-  },
-  {
-    name: "Tests",
-    value: 0.381,
-  },
-  {
-    name: "SEO",
-    value: 0.669,
-  },
-  {
-    name: "PHP",
-    value: 0.327,
-  },
-  {
-    name: "Python",
-    value: 0.725,
-  },
-  {
-    name: "Django",
-    value: 0.621,
-  },
-  {
-    name: "Flask",
-    value: 0.654,
-  },
-  {
-    name: "SQL",
-    value: 0.263,
-  },
-  {
-    name: "Git",
-    value: 0.847,
-  },
-  {
-    name: "Bash",
-    value: 0.483,
-  },
-  {
-    name: "API",
-    value: 0.945,
-  },
-  {
-    name: "English",
-    value: 0.4799999999999999999,
-  },
-  {
-    name: "Design",
-    value: 0.661,
-  },
-].forEach((skill) =>
+database.skills.map((skill) =>
   document.querySelector(".skills").insertAdjacentHTML(
     "beforeend",
     `

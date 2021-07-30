f5f7f70973f85e61bdb15b48b6db7d2d764a7e22
pin-cup <user@mailfence.com>
2021-07-30 18:39:54 +0300

    ForEach on Map, destructuring, add title

--- src/components/Portfolio/projects/main.js
+++ src/components/Portfolio/projects/main.js
@@ -1,37 +1,40 @@
 // ## Projects
 "use strict";
 
-portfolioWorksDB.forEach((project_arr) =>
+database.portfolio.map(({ title, tags, desc, url_demo, url_github, url_img }) =>
   projects.insertAdjacentHTML(
     "beforeend",
     `
-    <div class="project ${project_arr.tags}">
+    <div class="project ${tags || ""}">
       <div class="project__cube-wrapper">
         <div class="project__cube">
-          <div class="project__cube-top">
-            <p class="project__desc">${project_arr.desc}</p>
 
+          <div class="project__cube-top">
+            <p class="project__desc">${desc || "Test descriptions"}</p>
             <div class="project__links">
               ${
-                project_arr.url_demo
-                  ? `<a class="project__link project__link-demo" href="${project_arr.url_demo}">demo</a>`
+                url_demo
+                  ? `<a class="project__link project__link-demo" href="${url_demo}">demo</a>`
                   : ""
               }
               ${
-                project_arr.url_github
-                  ? `<a a class= "project__link project__link-github" href = "${project_arr.url_github}">github</a>`
+                url_github
+                  ? `<a a class= "project__link project__link-github" href = "${url_github}">github</a>`
                   : ""
               }
             </div>
           </div>
 
           <div class="project__cube-front">
-            <img class="project__img" src="${
-              project_arr.url_img ? project_arr.url_img : "./src/components/Portfolio/projects/static.webp"
-            }" alt="Picture - ${project_arr.url_img}">
+            <h2 class="project__title">${title || "Test Title"}</h2>
+            <img class="project__front-img" src="${
+              url_img || "./src/components/Portfolio/projects/static.webp"
+            }" alt="Picture - ${url_img}">
           </div>
+
         </div>
       </div>
-    </div>`
+    </div>
+    `
   )
 );

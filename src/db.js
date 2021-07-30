d0837b8f6e5a14147a77a592fd052646bf571f6f
pin-cup <user@mailfence.com>
2021-07-30 17:58:32 +0300

    Add "DB"

---
+++ src/db.js
@@ -0,0 +1,163 @@
+const database = {
+  portfolio: [
+    {
+      title: "Native Crypto Coin Calculator",
+      url_demo: "https://vivy-eye.github.io/Lend-CryptoCoin-Calculator/",
+      url_github: "https://github.com/Vivy-Eye/Lend-CryptoCoin-Calculator/",
+      url_img:
+        "https://repository-images.githubusercontent.com/349825660/4a1f1600-9bb1-11eb-9858-5ab48b14a38f",
+      tags: "static javascript spa api",
+      desc: `Тестовое задание.\nSPA - конвертер криптовалют.\nAPI - https://www.coingecko.com/ru/api.\nНаписан без фреймворков и библиотек.`,
+    },
+
+    {
+      title: "Native CRUD Password Manager",
+      url_demo: "https://vivy-eye.github.io/Lend-CRUD-Passoword-Manager/",
+      url_github: "https://github.com/Vivy-Eye/Lend-CRUD-Passoword-Manager/",
+      url_img:
+        "https://user-images.githubusercontent.com/70973663/123513295-196d5980-d695-11eb-9093-c810a55a0cad.png",
+      tags: "static javascript spa",
+      desc: "Мини приложения для создания и расширения таблиц, в том числе поролей. Таблицы сохраняються как файл .json после нажатия на конпку Create File и ссылки на скачивание файла.",
+    },
+
+    {
+      title: "Telegram-Bot-Parser",
+      url_github: "https://github.com/Vivy-Eye/pip-Telegram-Bot-Parser",
+      tags: "python bot",
+      desc: "Python bot для работы с api",
+    },
+
+    {
+      title: "Jeanhy-Administrator",
+      url_demo: "https://jeanhy-administrator.vercel.app/",
+      url_github: "https://github.com/Vivy-Eye/Jeanhy-Administrator",
+      url_img:
+        "https://user-images.githubusercontent.com/70973663/126912405-cc149f7f-fcf7-4a58-8c1d-5c701a8c0765.png",
+      tags: "nextjs react spa javascript",
+      desc: "Admin-panel Template on Next.js",
+    },
+    {
+      title: "Next-Profiling",
+      url_demo: "",
+      url_github: "https://github.com/Vivy-Eye/Next-Profiling",
+      url_img: "",
+      tags: "nextjs react spa javascript",
+      desc: "Simple Profile page",
+    },
+
+    {
+      title: "",
+      url_demo: "",
+      url_github: "",
+      url_img: "",
+      desc: "",
+      tags: "",
+    },
+
+    /*
+    {
+      title: "",
+      url_demo: "",
+      url_github: "",
+      url_img: "",
+      desc: "",
+      tags: "",
+    },
+    */
+  ],
+  about: [
+    `
+        I can:
+      - create SPA in vanilla JavaScript;
+      - work with Git, Bash, code formatting systems;
+      - work with API (Vkontakte, Telegram and others, less popular);
+    `,
+    `
+        I have:
+      - More than four-year experience of "home projects";
+      - experience in design and animating interfaces;
+      - experience in creating parsers and bots;
+      - experience in creating a back-end;
+      - Experience with React (SPA), Node.js (npm), Flask and Django (back-end), PHP (not much experience);
+    `,
+    `
+        I am able:
+      - to understand the architecture of the project;
+      - to do work all my free time;
+      - to study the necessary tools by myself.
+    `,
+    `
+      I am well organized. Ready to work.
+    `,
+  ],
+  skills: [
+    {
+      name: "HTML",
+      value: 0.884,
+    },
+    {
+      name: "CSS",
+      value: 0.819,
+    },
+    {
+      name: "JavaScript",
+      value: 0.901,
+    },
+    {
+      name: "Node.js",
+      value: 0.291,
+    },
+    {
+      name: "React",
+      value: 0.418,
+    },
+    {
+      name: "Tests",
+      value: 0.381,
+    },
+    {
+      name: "SEO",
+      value: 0.669,
+    },
+    {
+      name: "PHP",
+      value: 0.327,
+    },
+    {
+      name: "Python",
+      value: 0.725,
+    },
+    {
+      name: "Django",
+      value: 0.621,
+    },
+    {
+      name: "Flask",
+      value: 0.654,
+    },
+    {
+      name: "SQL",
+      value: 0.263,
+    },
+    {
+      name: "Git",
+      value: 0.847,
+    },
+    {
+      name: "Bash",
+      value: 0.483,
+    },
+    {
+      name: "API",
+      value: 0.945,
+    },
+    {
+      name: "English",
+      value: 0.4799999999999999999,
+    },
+    {
+      name: "Design",
+      value: 0.661,
+    },
+  ],
+};
